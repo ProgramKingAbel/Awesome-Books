a606ca9145f362ced7c3e16767b4eda1621c607b
ProgramKingAbel <abelmorara254@gmail.com>
2023-05-01 17:55:14 +0100

    Solved styleLint error

--- script.js
+++ script.js
@@ -1,60 +1,53 @@
 const books = localStorage.getItem('book') ? JSON.parse(localStorage.getItem('book')) : [];
 
-console.log(books);
-
 const addBook = (e) => {
-    e.preventDefault();
+  e.preventDefault();
 
-    let book = {
-        id: Date.now(),
-        title: document.getElementById('title').value,
-        author: document.getElementById('author').value,
-    }
-    if (book.id && book.title && book.author) {
-        books.push(book);
-    }
-    document.querySelector('form').reset();
+  const book = {
+    id: Date.now(),
+    title: document.getElementById('title').value,
+    author: document.getElementById('author').value,
+  };
+  if (book.id && book.title && book.author) {
+    books.push(book);
+  }
+  document.querySelector('form').reset();
 
-    localStorage.setItem('book', JSON.stringify(books));
-    location.reload();
-}
+  localStorage.setItem('book', JSON.stringify(books));
+  document.location.reload();
+};
 
 books.forEach((element, e) => {
-    const allBooks = document.querySelector('.all-books');
-    const book = document.createElement('div');
-    book.classList = 'book';
+  const allBooks = document.querySelector('.all-books');
+  const book = document.createElement('div');
+  book.classList = 'book';
 
-    book.innerHTML = `
+  book.innerHTML = `
     
     <h2 class="title">${books[e].title}</h2>
-    <p class="author">${books[e].author}</p>
+    <p class="author">Author: <span></span>${books[e].author}</p>
     <button class="remove">Remove</button>
     <hr />
     `;
-    allBooks.append(book);
-    
+  allBooks.append(book);
 });
 
-function activateDelete() {
-    let remove = document.querySelectorAll('.remove');
-    console.log(remove);
-    remove.forEach((btn, i) => {
-        btn.addEventListener('click', () => { deleteBook(i) })
-               
-    })
-}
-
-//filter function
+// filter function
 
 function deleteBook(i) {
-    books.splice(i, 1);
-    localStorage.setItem('book', JSON.stringify(books));
-    location.reload();
+  books.splice(i, 1);
+  localStorage.setItem('book', JSON.stringify(books));
+  document.location.reload();
 }
 
+function activateDelete() {
+  const remove = document.querySelectorAll('.remove');
+  remove.forEach((btn, i) => {
+    btn.addEventListener('click', () => { deleteBook(i); });
+  });
+}
 
 document.addEventListener('DOMContentLoaded', () => {
-    document.getElementById('submit').addEventListener('click', addBook);
-    activateDelete();
-   
-})
+  document.getElementById('submit').addEventListener('click', addBook);
+  activateDelete();
+});
