c21154384088a772ac98a204c49b6d2bacf5012a
ProgramKingAbel <abelmorara254@gmail.com>
2023-05-03 13:06:22 +0100

    Added required sections

--- script.js
+++ script.js
@@ -1,4 +1,65 @@
 /* eslint-disable max-classes-per-file */
+
+// Time code 
+const currentTime = document.querySelector('.current-time');
+
+function showTime() {
+  let time = new Date();
+  currentTime.innerHTML = time.toLocaleTimeString('en-US', { hour12: true });
+  setTimeout(showTime, 1000);
+}
+showTime();
+
+//Date Code
+
+function currentDate() {
+  let now = new Date();
+
+  //return integer
+  let dayName = now.getDay(),
+    dayNum = now.getDate(),
+    month = now.getMonth(),
+    year = now.getFullYear();
+  
+  const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  const weekDay = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+
+  //collect ids of date
+  const collect = ["day", "daynum", "month", "year"];
+
+  // return value array with number as a index
+  const val = [weekDay[dayName], dayNum, months[month], year];
+
+  for (var i = 0; i < collect.length; i++) {
+    document.getElementById(collect[i]).firstChild.nodeValue = val[i];
+  }
+}
+currentDate();
+
+
+
 // Book class to represent a book
 class Book {
   constructor(id, title, author) {
@@ -19,7 +80,7 @@ class Store {
     const books = Store.getBooks();
     books.push(book);
     localStorage.setItem('books', JSON.stringify(books));
-    document.location.reload();
+    window.alert('record added');
   }
 
   static removeBook(i) {
@@ -27,6 +88,7 @@ class Store {
     books.splice(i, 1);
     localStorage.setItem('books', JSON.stringify(books));
     document.location.reload();
+    
   }
 }
 
@@ -97,4 +159,7 @@ function activateDelete() {
 
 document.addEventListener('DOMContentLoaded', () => {
   activateDelete();
+  disappear();
+
 });
+
